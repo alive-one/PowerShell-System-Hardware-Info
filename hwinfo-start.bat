e95450bd1e6422bb0b6fc8a018169e8e3c8c410f
alive-one <88646366+alive-one@users.noreply.github.com>
2024-04-07 00:38:27 +0800

    Update hwinfo-start.bat
    Updated for compatibility with older versions of Windows 10, like 1803

--- hwinfo-start.bat
+++ hwinfo-start.bat
@@ -1,8 +1,8 @@
 rem | Allow execution of hwinfo.ps1 (Administrator priviliges reuired)
-powershell.exe -ExecutionPolicy Bypass -File .\hwinfo.ps1
-
-rem | Start hwinfo.ps1 from 
-powershell.exe -noexit -file "%~dp0hwinfo.ps1" 
-
+powershell.exe Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
+  
+rem | Start hwinfo.ps1 from
+powershell.exe -noexit -file "%~dp0hwinfo.ps1"
+  
 rem | Restore Default Execution Policy
-powershell Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Default
+powershell.exe Set-ExecutionPolicy -Scope CurrentUser Default
